9e81daf84e05f1c1c69c1e7e533939e80406ab86
dokimion24 <leecy3610@gmail.com>
2023-07-25 13:38:29 +0900

    feat: navbar 추가

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import React from 'react';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import HomeIcon from './ui/icons/HomeIcon';
+import HomeFillIcon from './ui/icons/HomeFillIcon';
+import SearchIcon from './ui/icons/SearchIcon';
+import SearchFillIcon from './ui/icons/SearchFillIcon';
+import NewIcon from './ui/icons/NewIcon';
+import NewFillIcon from './ui/icons/NewFillIcon';
+
+export default function Navbar() {
+  const pathname = usePathname();
+  console.log(pathname);
+
+  const menu = [
+    {
+      href: '/',
+      icons: <HomeIcon />,
+      clickedIcons: <HomeFillIcon />,
+    },
+    {
+      href: '/search',
+      icons: <SearchIcon />,
+      clickedIcons: <SearchFillIcon />,
+    },
+    {
+      href: '/new',
+      icons: <NewIcon />,
+      clickedIcons: <NewFillIcon />,
+    },
+  ];
+
+  return (
+    <div>
+      <Link href="/">
+        <h1>Instargram</h1>
+      </Link>
+      <nav>
+        <ul>
+          {menu.map((item) => (
+            <li key={item.href}>
+              <Link href={item.href}>
+                {pathname === item.href ? item.clickedIcons : item.icons}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
+}
