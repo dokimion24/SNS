4f788e9e7433b34f3630d85e98a55a1a20e29b5f
dokimion24 <leecy3610@gmail.com>
2023-07-29 23:48:21 +0900

    feat: PostList 컴포넌트 작성

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,5 +1,22 @@
-import React from 'react';
+'use client';
 
-export default function PostList() {
-  return <>PostList</>;
+import { SimplePost } from '@/model/post';
+import { User } from '@/model/user';
+import React from 'react';
+import useSWR from 'swr';
+
+type Props = {
+  user: User;
+};
+
+export default function PostList({ user: { name, username, image } }: Props) {
+  const { data: posts, isLoading } = useSWR<SimplePost[]>('/api/posts');
+
+  console.log(posts);
+
+  return (
+    <ul className='w-full p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px]'>
+      {posts && posts.map((post) => <li key={post.id}>{post.text}</li>)}
+    </ul>
+  );
 }
