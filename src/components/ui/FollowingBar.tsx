9d29ffbb34d03e33838e68a05d95ccdd40103030
dokimion24 <leecy3610@gmail.com>
2023-07-29 16:15:03 +0900

    feat: 팔로잉 목록 가져와 보여주기

--- src/components/ui/FollowingBar.tsx
+++ src/components/ui/FollowingBar.tsx
@@ -1,5 +1,34 @@
+'use client';
+
+import { DetailUser } from '@/model/user';
 import React from 'react';
+import { PropagateLoader } from 'react-spinners';
+import useSWR from 'swr';
+import Link from 'next/link';
+import Avatar from './Avatar';
 
 export default function FollowingBar() {
-  return <>Follwing</>;
+  const { data, isLoading, error } = useSWR<DetailUser>('/api/me');
+  const users = data?.following;
+  return (
+    <section>
+      {isLoading ? (
+        <PropagateLoader size={8} />
+      ) : (
+        (!users || users.length === 0) && <p>{`You don't have follwing`}</p>
+      )}
+      {users && users.length > 0 && (
+        <ul>
+          {users.map(({ image, username }) => (
+            <li key={username}>
+              <Link href={`/user/${username}`}>
+                <Avatar image={image} highlight />
+                <p>{username}</p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
 }
