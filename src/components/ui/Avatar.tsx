95beb0e961e66f328eda82bb2f418b7e96ccf9b4
dokimion24 <leecy3610@gmail.com>
2023-07-29 14:24:21 +0900

    feat: props 추가, 스타일링

--- src/components/ui/Avatar.tsx
+++ src/components/ui/Avatar.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 
-export default function Avatar({ image }: { image?: string | null }) {
+type Props = {
+  image?: string | null;
+  size?: 'small' | 'normal';
+  highlight?: boolean;
+};
+
+export default function Avatar({
+  image,
+  size = 'normal',
+  highlight = false,
+}: Props) {
   return (
-    <div>
+    <div className={getContainerStyle(size, highlight)}>
       {/* eslint-disable-next-line @next/next/no-img-element*/}
       <img src={image ?? undefined} alt='profile' />
     </div>
   );
+}
+
+function getContainerStyle(size: string, highlight: boolean) {
+  const baseStyle = 'rounded-full';
+  const highlightStyle = highlight
+    ? 'bg-gradient-to-bl from-fuchsia-600 via-rose-500'
+    : '';
+  const sizeStyle = size === 'small' ? 'w-9 h-9' : 'w-[68px] h-[68px]';
+  return `${baseStyle} ${highlightStyle} ${sizeStyle}`;
 }
