2f20bf9a47e7650af09b66d763dc19650c41760e
dokimion24 <leecy3610@gmail.com>
2023-08-01 11:52:24 +0900

    feat: 컴포넌트 생성

---
+++ src/components/UserCard.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import React from 'react';
+import Avatar from './Avatar';
+import { UserSearchResult } from '@/model/user';
+import Link from 'next/link';
+
+type Props = {
+  user: UserSearchResult;
+};
+
+export default function UserCard({ user }: Props) {
+  return (
+    <Link
+      href={`/user/${user.username}`}
+      className='flex items-center w-full rounded-sm border-neutral-300 mb-2 p-4 bg-white hover:bg-neutral-50'>
+      <Avatar image={user.image} size='medium' />
+      <div className='text-neutral-500'>
+        <p className='text-black font-bold leading-4'>{user.username}</p>
+        <p>{user.name}</p>
+        <p className='text-sm leading-4'>
+          {user.followers ?? 0} followers {user.following ?? 0} following
+        </p>
+      </div>
+    </Link>
+  );
+}
---
+++ src/components/UserSearch.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import React, { FormEvent, useState } from 'react';
+import useSWR from 'swr';
+import GridSpinner from './ui/GridSpinner';
+import Avatar from './Avatar';
+import { UserSearchResult } from '@/model/user';
+import UserCard from './UserCard';
+
+export default function UserSearch() {
+  const [keyword, setKeyword] = useState('');
+
+  const {
+    data: users,
+    isLoading,
+    error,
+  } = useSWR<UserSearchResult[]>(`/api/search/${keyword}`);
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
+  return ( 
+    <section className='w-full flex flex-col items-center max-w-2xl my-4'>
+      <form onSubmit={onSubmit} className='w-full'>
+        <input
+          className='w-full text-xl p-3 outline-none border border-gray-400'
+          type='text'
+          autoFocus
+          placeholder='Search'
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+      </form>
+      {isLoading && (
+        <div className='text-center mt-32'>
+          <GridSpinner />
+        </div>
+      )}
+      {error && <p>에러</p>}
+      {!isLoading && !error && users?.length === 0 && <p>찾는 사용자가 없음</p>}
+      {users && users.length > 0 && (
+        <ul className='w-full p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px]'>
+          {users.map((user) => (
+            <li key={user.username} className='mb-4'>
+              <UserCard user={user} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
+}
