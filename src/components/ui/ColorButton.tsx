8aec8e094e007392514153759373f37a249e8cf4
dokimion24 <leecy3610@gmail.com>
2023-08-04 17:36:22 +0900

    feat: ColorButton 컴포넌트 수정

--- src/components/ui/ColorButton.tsx
+++ src/components/ui/ColorButton.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 
 type Props = {
   text: string;
+  color: string;
   onClick: () => void;
 };
 
-export default function ColorButton({ text, onClick }: Props) {
+export default function ColorButton({ text, onClick, color }: Props) {
   return (
-    <div className="rounded-md bg-gradient-to-bl">
-      <button onClick={onClick}>{text}</button>
+    <div className='rounded-md bg-gradient-to-bl'>
+      <button
+        className={`text-white font-bold rounded-md py-2 px-8 leading-4 ${color}`}
+        onClick={onClick}>
+        {text}
+      </button>
     </div>
   );
 }
